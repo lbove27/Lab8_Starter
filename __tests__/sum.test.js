aee749f58debc99142666f10cd5a40f5785ab081
lbove27 <lbove@ucsd.edu>
2021-11-13 15:53:04 -0800

    done

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,5 +1,11 @@
 // sum.test.js
-
+const sum = require('../scripts/sum.js');
 test('adds 1 + 2 to equal 3', () => {
-  // TODO
+    expect(sum(1, 2)).toBe(3);
 });
+
+
+
+
+
+const { exportAllDeclaration } = require('@babel/types');
